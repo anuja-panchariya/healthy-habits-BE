4dc39f26a1f1dd7556ad250be718bb96ce964e38
Anuja Panchariya <anujapanchariya7@gmail.com>
2026-03-02 19:17:03 +0530

    Update habitRoutes.js

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -1,19 +1,19 @@
-import express from "express"
-import { requireAuth } from "@clerk/express"
-import { 
+// src/routes/habitRoutes.js - ALL ROUTES DEFINED!
+import express from 'express'
+import {
   getHabits,
   createHabit,
   deleteHabit,
   logHabit,
-  getWellnessScore  
-} from "../controllers/habitController.js"
+  getWellnessScore
+} from '../controllers/habitController.js'
 
 const router = express.Router()
 
-router.get('/', requireAuth(), getHabits)
-router.post('/', requireAuth(), createHabit)
-router.delete('/:id', requireAuth(), deleteHabit)
-router.post('/:id/log', requireAuth(), logHabit)
-router.get('/wellness-score', requireAuth(), getWellnessScore)
+router.get('/habits', getHabits)                    // GET habits
+router.post('/habits', createHabit)                 // POST new habit  
+router.delete('/habits/:id', deleteHabit)           // DELETE habit
+router.post('/habits/:id/log', logHabit)            // Log habit
+router.get('/habits/wellness-score', getWellnessScore)  // Wellness score
 
 export default router
